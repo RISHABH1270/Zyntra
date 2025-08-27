814df583cf3c7ada40e25009da7abe1a8d49ea92
Rishabh Singh <risu418rs@gmail.com>
2025-08-28 01:32:29 +0530

    MainCarousel

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { MainCarouselData } from './MainCarouselData';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
+const MainCrosel = () => {
+  const items = MainCarouselData.map((item) => 
+    <img className='cursor-pointer' role='presentation' src={item.image} alt='' />
+  );
 
-const MainCarousel = () => (
+  return (
     <AliceCarousel
-        mouseTracking
-        items={items}
-        controlsStrategy="alternate"
+      items={items}
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={3000}
+      infinite
     />
-);
+  );
+}
 
-export default MainCarousel;
+export default MainCrosel;
